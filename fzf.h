5117c33320931c38db8d3e65d0d2c96268c51515
Duc <duc@Ducs-MacBook-Air.local>
2026-09-07 01:08:32 -0700

    Support fzf exact-boundary terms

--- fzf.h
+++ fzf.h
@@ -115,6 +115,11 @@ fzf_result_t fzf_fuzzy_match_v2(bool case_sensitive, bool normalize,
 fzf_result_t fzf_exact_match_naive(bool case_sensitive, bool normalize,
                                    fzf_string_t *text, fzf_string_t *pattern,
                                    fzf_position_t *pos, fzf_slab_t *slab);
+fzf_result_t fzf_exact_match_boundary(bool case_sensitive, bool normalize,
+                                      fzf_string_t *text,
+                                      fzf_string_t *pattern,
+                                      fzf_position_t *pos,
+                                      fzf_slab_t *slab);
 fzf_result_t fzf_prefix_match(bool case_sensitive, bool normalize,
                               fzf_string_t *text, fzf_string_t *pattern,
                               fzf_position_t *pos, fzf_slab_t *slab);
@@ -172,6 +177,9 @@ int32_t utf8_fuzzy_index(fzf_string_t *input, const char *pattern,
 fzf_result_t fzf_exact_match_utf8(bool case_sensitive, bool normalize,
                                   fzf_string_t *text, fzf_string_t *pattern,
                                   fzf_position_t *pos, fzf_slab_t *slab);
+fzf_result_t fzf_exact_match_boundary_utf8(
+    bool case_sensitive, bool normalize, fzf_string_t *text,
+    fzf_string_t *pattern, fzf_position_t *pos, fzf_slab_t *slab);
 fzf_result_t fzf_prefix_match_utf8(bool case_sensitive, bool normalize,
                                    fzf_string_t *text, fzf_string_t *pattern,
                                    fzf_position_t *pos, fzf_slab_t *slab);
